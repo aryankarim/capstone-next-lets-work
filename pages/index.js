dcd91078912a88fd1628f8a0c72798d827ed59a4
aryanmajeed <aryanmajid97@gmail.com>
2022-02-06 17:49:00 +0300

    feat(SEO): add SEO to index page

--- pages/index.js
+++ pages/index.js
@@ -47,7 +47,22 @@ export default function Home({
   }, [dispatch]);
   return (
     <Box dir={router.locale === "ar" ? "rtl" : "ltr"} mb="20">
-      <CustomHead title="Home" />
+      <CustomHead title="Home" >
+      <meta name="title" content="Let's Work"/>
+      <meta name="description" content="Connect with people who are driven by the same idea as yours."/>
+
+      <meta property="og:type" content="website"/>
+      <meta property="og:url" content="https://letsswork.vercel.app/"/>
+      <meta property="og:title" content="Let's Work"/>
+      <meta property="og:description" content="Connect with people who are driven by the same idea as yours."/>
+      <meta property="og:image" content="https://letsswork.vercel.app/images/Team-amico%201.svg"/>
+
+      <meta property="twitter:card" content="summary_large_image"/>
+      <meta property="twitter:url" content="https://letsswork.vercel.app/"/>
+      <meta property="twitter:title" content="Let's Work"/>
+      <meta property="twitter:description" content="Connect with people who are driven by the same idea as yours."/>
+      <meta property="twitter:image" content="https://letsswork.vercel.app/images/Team-amico%201.svg"/>
+      </CustomHead>
       <TopSection />
       <Stack spacing="6" align="center">
         <PostList
